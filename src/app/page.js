e8bf29413eab6f4d736841c6243b6c29d70e255a
ceoDemitri <demichole@gmail.com>
2025-11-04 23:59:56 -0600

    here

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import bg from "./img/mainbg.jpeg";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="bg-[url(/img/mainbg.jpeg)] bg-cover bg-center"></div>
+      <div className="relative h-screen w-screen overflow-hidden">
+        <Image
+          src={bg}
+          alt="Giant Museum Background"
+          width={1920}
+          height={1080}
+          className="absolute -z-10 h-full w-full object-cover"
+        />
+      </div>
       <h1 className="text-5xl text-center">Giant Musuem</h1>
     </main>
   );
