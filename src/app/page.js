0e00324bc6d403eb9314d7e13fa916e3a8fe242f
ceoDemitri <demichole@gmail.com>
2025-11-04 23:48:32 -0600

    meant to start server

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="bg-[url(/img/mainbg.jpeg)] h-full w-full bg-center"></div>
+      <div className="bg-[url(/img/mainbg.jpeg)] bg-cover bg-center"></div>
       <h1 className="text-5xl text-center">Giant Musuem</h1>
     </main>
   );
