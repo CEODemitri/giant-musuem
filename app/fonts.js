7e716e5dd0e3539b226781943209a0359a0df65c
ceoDemitri <demichole@gmail.com>
2025-12-02 23:30:08 -0600

    refactor: adds bricolage to replace cinzel as header font. responsive body padding. updates colors to be more museumy

--- app/fonts.js
+++ app/fonts.js
@@ -1,13 +1,13 @@
-import { Cinzel, Lato } from "next/font/google";
+import { Bricolage_Grotesque, Lato } from "next/font/google";
 
-export const cinzel = Cinzel({
-    weight: ["400", "600", "900"],
-    subsets: ["latin"],
-    variable: "--font-header",
+export const bricolage = Bricolage_Grotesque({
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+  subsets: ["latin"],
+  variable: "--font-header",
 });
 
 export const lato = Lato({
-    weight: ["100", "300", "400", "700", "900"],
-    subsets: ["latin"],
-    variable: "--font-body",
+  weight: ["100", "300", "400", "700", "900"],
+  subsets: ["latin"],
+  variable: "--font-body",
 });
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import "./globals.css";
-import { cinzel, lato } from "./fonts";
-
+import { bricolage, lato } from "./fonts";
 
 export const metadata = {
   title: "Giant Museum",
@@ -21,32 +20,42 @@ let currentTime = `${currentHours}:${currentMinutes}`;
 function NavBar() {
   return (
     <header className="fixed top-0 left-0 right-0 z-50">
-        <nav className="absolute top-0 left-0 w-full flex items-end justify-between px-6 bg-[var(--background)] bg-opacity-70 z-10">
-            <div>
-                <p className="text-2xl font-bold">
-                    <a href="/">Giant Museum</a>
-                </p>
-                <ul className="flex space-x-6 text-[var(--foreground)]">
-                    <li className="hover:underline cursor-pointer"><a href={"./exhibits"}>Exhibits</a></li>
-                    <li className="hover:underline cursor-pointer"><a href={"./calendar"}>Calendar</a></li>
-                    <li className="hover:underline cursor-pointer"><a href={"./contact"}>Contact</a></li>
-                </ul>
-            </div>
-            <div>
-                <button className="bg-accent px-4 py-1 rounded-md text-white tracking-wider uppercase">Tickets</button>
-            </div>
-        </nav>
-        <div className="text-sm relative top-[6vh] left-[85vw] z-10 border border-b-neutral-300 w-fit p-2 rounded-md">
-            {/* day info: temp, time, etc */}
-            <p>London, Jotunheim</p>
-            <p className="text-[var(--foreground)]">
-                <span>72°F</span> |  <span>Sunny</span> |{" "}
-                <span>🌙</span>
-            </p>
-            <p>
-                <span>{fullDate}</span> | <span>{currentTime} {currentHours >= 12 ? 'PM' : 'AM'}</span>
-            </p>
+      <nav className="absolute top-0 left-0 w-full flex items-end justify-between px-6 bg-[var(--background)] bg-opacity-70 z-10">
+        <div>
+          <p className="text-2xl font-bold">
+            <a href="/">Giant Museum</a>
+          </p>
+          <ul className="flex space-x-6 text-[var(--foreground)]">
+            <li className="hover:underline cursor-pointer">
+              <a href={"./exhibits"}>Exhibits</a>
+            </li>
+            <li className="hover:underline cursor-pointer">
+              <a href={"./calendar"}>Calendar</a>
+            </li>
+            <li className="hover:underline cursor-pointer">
+              <a href={"./contact"}>Contact</a>
+            </li>
+          </ul>
         </div>
+        <div>
+          <button className="bg-accent px-4 py-1 rounded-md text-white tracking-wider uppercase">
+            Tickets
+          </button>
+        </div>
+      </nav>
+      <div className="text-sm relative top-[6vh] left-[85vw] z-10 border border-b-neutral-300 w-fit p-2 rounded-md">
+        {/* day info: temp, time, etc */}
+        <p>London, Jotunheim</p>
+        <p className="text-[var(--foreground)]">
+          <span>72°F</span> | <span>Sunny</span> | <span>🌙</span>
+        </p>
+        <p>
+          <span>{fullDate}</span> |{" "}
+          <span>
+            {currentTime} {currentHours >= 12 ? "PM" : "AM"}
+          </span>
+        </p>
+      </div>
     </header>
   );
 }
@@ -55,12 +64,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={` ${cinzel.variable} ${lato.variable} antialiased`}
+        className={`${lato.className} bg-[var(--background)] text-[var(--foreground)] min-h-screen px-2 md:px-6 lg:px-12 pb-12`}
       >
         <NavBar />
-        <div className="text-sm md:text-base leading-snug">
-            {children}
-        </div>
+        <div className="text-sm md:text-base leading-snug">{children}</div>
       </body>
     </html>
   );
